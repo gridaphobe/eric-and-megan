c8b98b1b76c534063b77becc0c04ea40ee5dc7bc
Eric Seidel <gridaphobe@gmail.com>
2014-07-06 12:33:00 -0700

    initial commit

---
+++ site.hs
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "index.html" $ do
+        route idRoute
+        compile $
+            getResourceBody
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
